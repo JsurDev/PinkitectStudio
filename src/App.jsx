58df3efd814b3ed0caedb0c36b58a8e44c8c4086
jesur <juansurianodev@gmail.com>
2026-08-09 20:28:09 -0600

    Primeros diseños de un sitio web dedicado al estilo de una chica que me gusta mucho

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./assets/svg/componentes/Header";
+
+function App() {
+  return (
+    <>
+      <div className="main-contenedor">
+        <Header />
+        <main className="layout"></main>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/svg/componentes/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Pinkitect Studio</h1>
+      <h3>Arquitecto in the making</h3>
+      <hr className="lineaDivisoria" />
+    </header>
+  );
+};
+
+export default Header;
